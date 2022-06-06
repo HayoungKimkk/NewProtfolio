12133bb6444e053ff7d08003ceb91c295a0099f3
HayoungKim <hayou94@gmail.com>
2022-06-06 18:12:43 +0900

    📄 Docs : Add Information for vrgame page

--- pages/works/newsbig.js
+++ pages/works/newsbig.js
@@ -1,31 +1,23 @@
 import {
   Container,
   Badge,
-  Link,
   List,
   ListItem,
 } from '@chakra-ui/react'
-import { ExternalLinkIcon } from '@chakra-ui/icons'
 import { Title, WorkImage, Meta } from '../../components/work'
 import P from '../../components/paragraph'
 import Layout from '../../components/layouts/article'
 
 const Work = () => (
-  <Layout title="IoTCity">
+  <Layout title="Newsbig">
     <Container>
       <Title>
-        빅데이터 뉴스분석 서비스 &#34신문물&#34 <Badge>2022</Badge>
+        빅데이터 뉴스분석 서비스 &#34;신문물&#34; <Badge>2022</Badge>
       </Title>
       <P>
         빅데이터 분석을 통해 유저가 원하는 뉴스를 추천해주는 서비스. Hardoop을 이용하여 Wordcount를 진행하였으며, 감정분석, 비중분석을 통해 각각의 기사가 가지고 있는 감정수치와 키워드를 추출하여 뉴스 추천에 사용하였음.
       </P>
       <List ml={4} my={4}>
-        <ListItem>
-          <Meta>Website</Meta>
-          <Link href="https://www.ismartcity.co.kr/default/mp3/mp3_sub1.php?sub=01/">
-          https://www.ismartcity.co.kr/ <ExternalLinkIcon mx="2px" />
-          </Link>
-        </ListItem>
         <ListItem>
           <Meta>Platform</Meta>
           <span>Linux</span>
--- pages/works/vrgame.js
+++ pages/works/vrgame.js
@@ -1,7 +1,6 @@
 import {
   Container,
   Badge,
-  Link,
   List,
   ListItem,
 } from '@chakra-ui/react'
@@ -10,10 +9,10 @@ import P from '../../components/paragraph'
 import Layout from '../../components/layouts/article'
 
 const Work = () => (
-  <Layout title="IoTCity">
+  <Layout title="Survival and Escape">
     <Container>
       <Title>
-        VR을 활용한 피란민 체험 게임 &#34Survival and Escape&#34 <Badge>2022</Badge>
+        VR을 활용한 피란민 체험 게임 &#34;Survival and Escape&#34; <Badge>2022</Badge>
       </Title>
       <P>
         최근 사회적으로 이슈가 되고 있는 전쟁 상황에서 피란민들이 느끼는 고통을 체험할 수 있는 VR 체험형 게임을 제작함. Unity를 이용해 게임을 제작하였으며 타겟 디바이스는 Meta Quest 2 를 이용하였음.
@@ -25,15 +24,24 @@ const Work = () => (
         </ListItem>
         <ListItem>
           <Meta>Stack</Meta>
-          <span>C#(Logic), Unity 2022(Game Engine)</span>
+          <span>Unity 2022(Game Engine), C#(Logic)</span>
         </ListItem>
       </List>
 
-      <WorkImage src="/images/works/temp_eyecatch.png" alt="temp" />
+      <WorkImage src="/images/works/vrgame_eyecatch.png" alt="VrGameTitleImage" />
 
       <p>
         유니티를 애용해 3DVR 게임을 구현하는게 프로젝트의 목표.<br/>
-        VR이기에 가능한 접근법들을 위해 팀원들과 2주(80시간)이상의 브레인스토밍을 진행함.<br/>
+        VR이기에 가능한 접근법들을 위해 팀원들과 2주(80시간)이상의 브레인스토밍을 진행함.<br/>a
+        개발을 위해 필요했던 영역은 그래픽, 인게임 로직이 있었다.
+        개발인원이 적었기 때문에 그래픽은 에셋을 구매하여 시간을 절약하기로 하고 로직적인 부분을 구현하기 위해 노력함.
+        내가 맡은 부분은 인벤토리 시스템과 아이템의 로직 구현.
+        유니티에서 이러한 부분을 구현하는데 있어 자주 쓰이는 기능인 Scriptable Object(이후 SO라 부름)를 이해하고 구현하는데 상당히 많은 시간을 소모함.
+        이는 기존 프로그래밍에서 OOP와 같은 방식(함수형 프로그래밍, 리액트의 컴포넌트 등)이라고 할 수 있음.
+        따라서 게임의 구성 요소로써 아이템과 인벤토리와 같은 시스템이 가져야 할 기능들을 구현하고 유기적으로 연결되게 하는 것이 필수적이었음.
+        이를 위해 서로 다른 오브젝트에서 상호작용할 수 있도록 데이터베이스 SO를 만들어 각 오브젝트의 ID값만으로 각각 아이템의 속성값을 참조할 수 있도록 하였음.
+        아이템과 인벤토리 시스템을 구현한 뒤 이를 UI에서 사용할 수 있도록 사용자 시점에 맞추어 Vector값이 조정되는 UI를 제작하였음.
+        
       </p>
 
     </Container>
